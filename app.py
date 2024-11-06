ab3e465dfc91ad82194f33f558ad6e110c3924b1
The Eye <165040642+theeye-network@users.noreply.github.com>
2024-11-06 06:39:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def registrations():
     return render_template('registrations.html', applicants=applicants)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
